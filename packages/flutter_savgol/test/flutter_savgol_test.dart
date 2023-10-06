7daa5ca49f75a87be72716d94c070437c19dfcab
Tom Picton <tom@tompicton.co.uk>
2023-10-05 20:02:33 -0400

    fix: Enable manual trigger of publish workflow

--- packages/flutter_savgol/test/flutter_savgol_test.dart
+++ packages/flutter_savgol/test/flutter_savgol_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Dummy test for ci', () {
-    // All tests are currently in the flutter_savgol example app.
+    // All tests are currently in the flutter_savgol example app
     expect(true, true);
   });
 }
--- packages/savgol/test/savgol_test.dart
+++ packages/savgol/test/savgol_test.dart
@@ -2,7 +2,7 @@ import 'package:test/test.dart';
 
 void main() {
   test('Dummy test for ci', () {
-    // All tests are currently in the flutter_savgol example app.
+    // All tests are currently in the flutter_savgol example app
     expect(true, true);
   });
 }
